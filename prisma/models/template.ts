beb59dcf75822560d79f3497be760df3b583803a
노지원 <dev.rojiwon@gmail.com>
2023-10-15 00:10:22 +0900

    ♻️ prisma util update

--- prisma/models/template.ts
+++ prisma/models/template.ts
@@ -1,156 +1,150 @@
-import { createModel } from "schemix";
+import { Description } from "../util/description";
+import { Table } from "../util/table";
 
-import { Comment } from "../util/comment";
-import { Model } from "../util/model";
+Table.create({
+    tableName: "users",
+    comments: Description.lines(
+        "User Root Entity",
+        "",
+        Description.namespace(),
+        Description.namespace("User"),
+        Description.author(),
+    ),
+})(
+    Table.addId(),
+    Table.addColumn("string")("name", {
+        comments: Description.lines("displayed username in service"),
+    }),
+    Table.addColumn("string")("image_url", {
+        optional: true,
+        comments: Description.lines("url path for profile image"),
+    }),
+    Table.addColumn("string")("email", {
+        optional: true,
+        comments: Description.lines("verified email address"),
+    }),
+    Table.setCreatable,
+    Table.setUpdatable,
+    Table.setDeletable,
+    Table.addRelation({ tableName: "articles", options: { list: true } }),
+    Table.addRelation({
+        tableName: "article_comments",
+        options: { list: true },
+    }),
+);
 
-export const User = createModel("UserModel", (model) => {
-    Model.define(
-        model,
-        Model.info(
-            "users",
-            ...Comment.lines("User Root Entity", ""),
-            Comment.namespace(),
-            Comment.namespace("User"),
-            Comment.author(),
+Table.create({
+    tableName: "articles",
+    comments: Description.lines(
+        "Article Root Entity",
+        "",
+        Description.namespace(),
+        Description.namespace("BBS"),
+        Description.author(),
+    ),
+})(
+    Table.addId(),
+    Table.addRelationalString("author")("users"),
+    Table.setCreatable,
+    Table.setDeletable,
+    Table.addRelation({
+        tableName: "article_snapshots",
+        fieldName: "snapshots",
+        options: { list: true },
+    }),
+    Table.addRelation({
+        tableName: "article_comments",
+        fieldName: "comments",
+        options: { list: true },
+    }),
+);
+
+Table.create({
+    tableName: "article_snapshots",
+    comments: Description.lines(
+        "Snapshot of Article",
+        "",
+        "a `article_snapshot` contains all content of the article.",
+        "",
+        "if article update body or title, a new article_snapshot is created.",
+        "",
+        Description.namespace(),
+        Description.namespace("BBS"),
+        Description.author(),
+    ),
+})(
+    Table.addId(),
+    Table.addRelationalString("article")("articles"),
+    Table.addColumn("string")("title", {
+        comments: Description.lines("title of article"),
+    }),
+    Table.addColumn("string")("body", {
+        comments: Description.lines(
+            "content of article",
+            "",
+            "content is only text with 20,000 limit",
         ),
-        Model.addId(),
-        Model.add("string")("name", {
-            comments: Comment.lines("displayed username in service"),
-        }),
-        Model.add("string")("image_url", {
-            optional: true,
-            comments: Comment.lines("url path for profile image"),
-        }),
-        Model.add("string")("email", {
-            optional: true,
-            comments: Comment.lines("verified email address"),
-        }),
-        Model.setCreatable,
-        Model.setUpdatable,
-        Model.setDeletable,
-        Model.add("relation")("articles", Article, { list: true }),
-        Model.add("relation")("article_comments", ArticleComment, {
+    }),
+    Table.setCreatable,
+);
+
+Table.create({
+    tableName: "article_comments",
+    comments: Description.lines(
+        "Article Comment Root Entity",
+        "",
+        Description.namespace(),
+        Description.namespace("BBS"),
+        Description.author(),
+    ),
+})(
+    Table.addId(),
+    Table.addRelationalString("article")("articles"),
+    Table.addRelationalString("parent", {
+        optional: true,
+        comments: Description.lines(
+            "if not null, a comment is reply of parent comment",
+        ),
+    })("article_comments", { name: "HierarchicalReply" }),
+    Table.addRelationalString("author")("users"),
+    Table.setCreatable,
+    Table.setDeletable,
+    Table.addRelation({
+        tableName: "article_comment_snapshots",
+        fieldName: "snapshots",
+        options: { list: true },
+    }),
+    Table.addRelation({
+        tableName: "article_comments",
+        fieldName: "children",
+        options: {
             list: true,
-        }),
-    );
-});
-
-export const Article = createModel("ArticleModel", (model) => {
-    Model.define(
-        model,
-        Model.info(
-            "articles",
-            ...Comment.lines("Article Root Entity", ""),
-            Comment.namespace(),
-            Comment.namespace("BBS"),
-            Comment.author(),
-        ),
-        Model.addId(),
-        Model.addRelationalString("author")(User, "users"),
-        Model.setCreatable,
-        Model.setDeletable,
-        Model.add("relation")("snapshots", ArticleSnapshot, { list: true }),
-        Model.add("relation")("comments", ArticleComment, { list: true }),
-    );
-});
-
-export const ArticleSnapshot = createModel("ArticleSnapshotModel", (model) => {
-    Model.define(
-        model,
-        Model.info(
-            "article_snapshots",
-            ...Comment.lines(
-                "Snapshot of Article",
-                "",
-                "a `article_snapshot` contains all content of the article.",
-                "",
-                "if article update body or title, a new article_snapshot is created.",
-                "",
-            ),
-            Comment.namespace(),
-            Comment.namespace("BBS"),
-            Comment.author(),
-        ),
-        Model.addId(),
-        Model.addRelationalString("article")(Article, "articles"),
-        Model.add("string")("title", {
-            comments: Comment.lines("title of article"),
-        }),
-        Model.add("string")("body", {
-            comments: Comment.lines(
-                "content of article",
-                "",
-                "content is only text with 20,000 limit",
-            ),
-        }),
-        Model.setCreatable,
-    );
-});
-
-export const ArticleComment = createModel("ArticleCommentModel", (model) => {
-    Model.define(
-        model,
-        Model.info(
-            "article_comments",
-            ...Comment.lines("Article Comment Root Entity", ""),
-            Comment.namespace(),
-            Comment.namespace("BBS"),
-            Comment.author(),
-        ),
-        Model.addId(),
-        Model.addRelationalString("article")(Article, "articles"),
-        Model.addRelationalString("parent", {
-            optional: true,
-            comments: Comment.lines(
-                "if not null, a comment is reply of parent comment",
-            ),
-        })(ArticleComment, "article_comments", {
             name: "HierarchicalReply",
-        }),
-        Model.addRelationalString("author")(User, "users"),
-        Model.setCreatable,
-        Model.setDeletable,
-        Model.add("relation")("snapshots", ArticleCommentSnapshot, {
-            list: true,
-        }),
-        Model.add("relation")("children", ArticleComment, {
-            list: true,
-            name: "HierarchicalReply",
-        }),
-    );
-});
+        },
+    }),
+);
 
-export const ArticleCommentSnapshot = createModel(
-    "ArticleCommentSnapshot",
-    (model) => {
-        Model.define(
-            model,
-            Model.info(
-                "article_comment_snapshots",
-                ...Comment.lines(
-                    "Snapshot of Article Comment",
-                    "",
-                    "a `article_comment_snapshot` contains all content of the comment.",
-                    "",
-                    "if comment update body or title, a new article_comment_snapshot is created.",
-                ),
-                Comment.namespace(),
-                Comment.namespace("BBS"),
-                Comment.author(),
-            ),
-            Model.addId(),
-            Model.addRelationalString("comment")(
-                ArticleComment,
-                "article_comments",
-            ),
-            Model.add("string")("content", {
-                comments: Comment.lines(
-                    "content of comment",
-                    "",
-                    "content is only text with 1,000 limit",
-                ),
-            }),
-            Model.setCreatable,
-        );
-    },
+Table.create({
+    tableName: "article_comment_snapshots",
+    comments: Description.lines(
+        "Snapshot of Article Comment",
+        "",
+        "a `article_comment_snapshot` contains all content of the comment.",
+        "",
+        "if comment update body or title, a new article_comment_snapshot is created.",
+        Description.namespace(),
+        Description.namespace("BBS"),
+        Description.author(),
+    ),
+})(
+    Table.addId(),
+    Table.addRelationalString("comment")("article_comments"),
+    Table.addColumn("string")("content", {
+        comments: Description.lines(
+            "content of comment",
+            "",
+            "content is only text with 1,000 limit",
+        ),
+    }),
+    Table.setCreatable,
 );
