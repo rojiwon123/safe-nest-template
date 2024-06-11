56aec417629b36e5cf60c775a8fbc04aae75aa19
rojiwon123 <dev.rojiwon@gmail.com>
2024-06-11 17:21:56 +0900

    chore: sync dependencies

--- packages/script/sync.js
+++ packages/script/sync.js
@@ -12,4 +12,8 @@ const api_package = JSON.parse(fs.readFileSync(api_path, 'utf-8'));
 
 api_package.version = root_package.version;
 
+api_package['dependencies']['typia'] = root_package['dependencies']['typia'];
+api_package['dependencies']['@nestia/fetcher'] =
+    root_package['dependencies']['@nestia/core'];
+
 fs.writeFileSync(api_path, JSON.stringify(api_package, null, 2), 'utf-8');
